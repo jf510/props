53713bc40d49a3626b573a8ae1a23f8a8bb15904
Jfontaine <jfontaine19@gmail.com>
2022-06-13 08:13:46 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import { choice, remove } from "./helpers";
+import fruits from "./fruits";
+
+let fruit = choice(fruits);
+
+console.log(`I'd like one ${fruit}, please`);
+console.log(`Here you go: ${fruit}`);
+console.log(`Delicious! May I have another?`);
+
